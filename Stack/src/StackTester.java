0f63c70f83bf2d2a46adbd06beff40ae283f2cfa
Prajyot5135 <130391558+prajyotpawar5135@users.noreply.github.com>
2023-07-04 00:54:08 +0530

    ~Stack Operations

---
+++ Stack/src/StackTester.java
@@ -0,0 +1,16 @@
+
+public class StackTester {
+
+	public static void main(String[] args) {
+
+		Stack<Integer> si=new Stack<Integer>(5);
+		
+		System.out.println(si.push(10));
+		System.out.println(si.push(20));
+
+		System.out.println(si.pop());
+
+		
+	}
+
+}
